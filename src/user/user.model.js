a7a8e447292b157661c41a70a01de02c0546996e
kasiasaif <kasia.saif@gmail.com>
2022-01-18 16:41:48 +0000

    user

--- src/user/user.model.js
+++ src/user/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -8,14 +9,20 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    unique: true,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
   },
 });
+
+userSchema.methods.generateAuthToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.SECRET, {
+    expiresIn: "365 days",
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
